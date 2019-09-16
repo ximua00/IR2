196c6a9349398dff768f9274e809f43e5f43e786
Manasa JB <43213734+jbmanasa@users.noreply.github.com>
2019-09-16 17:26:24 +0200

    Updated BaseData
    Preprocess and split data into train, validation and test data.

--- BaseData.py
+++ BaseData.py
@@ -1,7 +1,10 @@
 import json
 from collections import defaultdict, Counter 
 from pprint import pprint
+import numpy as np
+import re
 
+np.random.seed(69)
 
 PAD_INDEX = 0
 PAD_WORD = '<PAD>'
@@ -19,34 +22,111 @@ UNK_WORD = '<UNK>'
 class BaseData:
     def __init__(self, data_dir):
         self.data_dir = data_dir
+        self.word2idx = {}
+        self.unk_frequency = 3
+
+        self.text_data = []
+
+        self.index_counter = 3
+
+        self.user_idx = {}
+        self.user_idx_counter = 0
+
+        self.item_idx = {}
+        self.item_idx_counter = 0
+
         self.data = self.parse_data()
+        self.word2idx, self.idx2word = self.make_mappings()
+        self.train_data, self.val_data, self.test_data = self.split_data()
 
     def parse_data(self):
         data = defaultdict(dict)
-        text_data = []
         with open(self.data_dir, 'r') as jsonfile:
             for reviewID, line in enumerate((jsonfile)):
                 line_data = json.loads(line)
-                data[reviewID]["userID"] = line_data["reviewerID"]
-                data[reviewID]["itemID"] = line_data["asin"]
-                data[reviewID]["helpful"] = line_data["helpful"]
-                data[reviewID]["reviewText"] = line_data["reviewText"]
-                [text_data.append(word) for word in line_data["reviewText"].split()]
-                data[reviewID]["rating"] = line_data["overall"]
 
-        self.word_counter = Counter(text_data)
-        print(self.word_counter)
+                if line_data["reviewerID"] in self.user_idx.keys():
+                    data[reviewID]["userID"] = self.user_idx[line_data["reviewerID"]]
+                else:
+                    self.user_idx_counter += 1
+                    data[reviewID]["userID"] = self.user_idx_counter
+                    self.user_idx[line_data["reviewerID"]] = self.user_idx_counter
+
+                if line_data["asin"] in self.user_idx.keys():
+                    data[reviewID]["itemID"] = self.item_idx[line_data["asin"]]
+                else:
+                    self.item_idx_counter += 1
+                    data[reviewID]["itemID"] = self.item_idx_counter
+                    self.item_idx[line_data["asin"]] = self.item_idx_counter
+
+                data[reviewID]["helpful"] = line_data["helpful"]
+                data[reviewID]["rating"] = line_data["overall"]
+                data[reviewID]["reviewText"] = line_data["reviewText"]
+
+                line_data["reviewText"] = line_data["reviewText"].lower()
+                text_split = re.split("[ .,;()&!]+", line_data["reviewText"])
+                [self.text_data.append(word) for word in text_split]
+                data[reviewID]["reviewSplitText"] = text_split
 
         return data
 
-    
-        
-    
-        
-            
+    def make_mappings(self):
+        word2idx = {}
+        idx2word = {}
 
+        word_counter = Counter(self.text_data)
 
+        for review_id in self.data.keys():
+            review_sentence_idxs = [START_INDEX]
+            for word in self.data[review_id]["reviewSplitText"]:
+                if word_counter[word] > self.unk_frequency:
+                    # Add to the vocabulary
+                    if word not in self.word2idx.keys():
+                        self.index_counter += 1
+                        word2idx[word] = self.index_counter
+                        idx2word[self.index_counter] = word
+                    # Add word idx to review
+                    review_sentence_idxs.append(self.index_counter)
+                else:
+                    review_sentence_idxs.append(UNK_INDEX)
+            # print(review_sentence_idxs)
+            review_sentence_idxs.append(END_INDEX)
+            self.data[review_id]['review'] = review_sentence_idxs
+        return word2idx, idx2word
+
+    def split_data(self, train_split = 0.7, val_split = 0.15, test_split = 0.15):
+        train_set = {}
+        val_set = {}
+        test_set = {}
+        data_size = len(self.data)
+        train_len = int(train_split * data_size)
+        val_len = int(val_split * data_size)
+        test_len = int(test_split * data_size)
+        review_ids = [i for i in range(1, data_size+1)]
+        review_ids = np.random.permutation(review_ids)
+        #Train set
+        for i in range(train_len):
+            train_set[review_ids[i]] = self.data[review_ids[i]]
+        #Validation set
+        for i in range(train_len, train_len+val_len):
+            val_set[review_ids[i]] = self.data[review_ids[i]]
+        #Test set
+        for i in range(train_len+val_len, data_size):
+            test_set[review_ids[i]] = self.data[review_ids[i]]
+
+        return train_set, val_set, test_set
 
 
 if __name__ == "__main__":
-    data = BaseData("./data/Musical_Instruments_5.json")
+    base_data = BaseData("./data/Musical_Instruments_5.json")
+
+    # basedata is an instance of the BaseData class
+    # base_data.train_data/val_data/test_data outputs a defaultdictionary with review_ids as key.
+    # each value contains {["userID"]:X, ["itemID"]:X, ["helpful"]:X, ["rating"]:X, ["review"]:X}
+    # userID is an index
+    # itemID is an index
+    # helpful is a list of [X,X]
+    # rating is a number from 0-5
+    # review is a list of indices corresponding to each word. Already with START and END of review.
+            # Thresholded at min 3 words, lowercased, NOT PADDED.
+
