0ed9fe0788f2437ff148a246eb27c032a376b540
Manasa JB <43213734+jbmanasa@users.noreply.github.com>
2019-09-21 18:32:01 +0200

    Doesn't work
    Just a push

--- K-GRAMS/model.py
+++ K-GRAMS/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class KGRAMS(nn.Module):
-    def __init__(self, embedding_size, vocab_size, out_channels, filter_size, review_length, user_id_size, item_id_size, embedding_id_size, hidden_size)    :
+    def __init__(self, embedding_size, vocab_size, out_channels, filter_size, review_length, user_id_size, item_id_size, embedding_id_size, hidden_size, latent_size)    :
         super(KGRAMS, self).__init__()
         self.word_embeddings = nn.Embedding(num_embeddings=vocab_size,
                                             embedding_dim=embedding_size)
@@ -12,14 +12,16 @@ class KGRAMS(nn.Module):
                                   review_length=review_length,
                                   score_size=item_id_size,
                                   id_size=user_id_size,
-                                  hidden_size=hidden_size)
+                                  hidden_size=hidden_size,
+                                  latent_size=latent_size)
         self.item_net = EntityNet(embedding_id_size=embedding_id_size,
                                   out_channels=out_channels,
                                   filter_size=filter_size,
                                   review_length=review_length,
                                   score_size=user_id_size,
                                   id_size=item_id_size,
-                                  hidden_size=hidden_size)
+                                  hidden_size=hidden_size,
+                                  latent_size=latent_size)
 
 
     def forward(self, target_user_id, user_revs, review_user_ids, target_item_id, item_revs, review_item_ids):
@@ -28,7 +30,7 @@ class KGRAMS(nn.Module):
 
 
 class EntityNet(nn.Module):
-    def __init__(self, embedding_id_size, out_channels, filter_size, review_length, score_size, id_size, hidden_size):
+    def __init__(self, embedding_id_size, out_channels, filter_size, review_length, score_size, id_size, hidden_size, latent_size):
         super(EntityNet, self).__init__()
         self.entity_score_embeddings = nn.Embedding(num_embeddings=score_size,
                                             embedding_dim=embedding_id_size)
@@ -47,36 +49,50 @@ class EntityNet(nn.Module):
         self.b2 = nn.Parameter(torch.rand(1))
         self.h = nn.Parameter(torch.rand(hidden_size))
 
+        self.softmax = nn.Softmax(dim=2)
+        self.linear = nn.Linear(out_channels, latent_size)
 
     def forward(self, target_id, reviews, review_ids, word_embeddings):
         attentions = []
         features = []
-        for review, review_id in zip(reviews, review_ids):
-            rev_embedding = word_embeddings(review)
-            input_embeddings = rev_embedding.view(-1, 1, rev_embedding.size(1), rev_embedding.size(2))
-            x = self.conv2d(input_embeddings)
-            x = self.relu(x)
-            review_feats = self.max_pool(x).squeeze() #4D : batch_size(1) * out_channels * height(1) * width(1)
-            review_feats = review_feats.view(-1, 1)
-            score_embedding = self.entity_score_embeddings(review_id).view(-1, 1)
-            review_attention = torch.mm(self.h.view(1, -1), self.relu(torch.mm(self.W_O, review_feats) + torch.mm(self.W_u, score_embedding) + self.b1.view(-1,1))) + self.b2
-            features.append(review_feats)
-            attentions.append(review_attention)
-            
-        return None
+        # for review, review_id in zip(reviews, review_ids):
+        reviews_stacked = torch.stack(reviews, dim=0)
+        print(reviews_stacked.size())
+        rev_embeddings = word_embeddings(reviews_stacked).view()
+        input_embeddings = rev_embeddings.view(rev_embeddings.size(0), 1, rev_embeddings.size(1), rev_embeddings.size(2))
+        x = self.conv2d(input_embeddings)
+        x = self.relu(x)
+        review_feats = self.max_pool(x).squeeze() #4D : batch_size(1) * out_channels * height(1) * width(1)
+        review_feats = review_feats.view(-1, 1)
+        score_embedding = self.entity_score_embeddings(review_id).view(-1, 1)
+        review_attention = torch.mm(self.h.view(1, -1), self.relu(torch.mm(self.W_O, review_feats) + torch.mm(self.W_u, score_embedding) + self.b1.view(-1,1))) + self.b2
+        features.append(review_feats)
+        attentions.append(review_attention)
+
+        attentions_stack = torch.stack(attentions, dim=2)  # batch_size * attentions * #reviews
+        attentions_stack = self.softmax(attentions_stack).view(1, -1)
+        features_stack = torch.stack(features, dim=2) #features * batch_size * #reviews
+        reviews_importance = (attentions_stack * features_stack).sum(dim=2).view(1, -1)
+        entity_features = self.linear(reviews_importance)
+        # print("review_feats size ", review_feats.size(), "features_stack size ", features_stack.size())
+        # print("review_attention size ", review_attention.size(), "attentions_stack size ", attentions_stack.size())
+        # print("reviews_importance size ", reviews_importance.size())
+        # print("Entity features size ", entity_features.size())
+        return entity_features
 
 if __name__ == "__main__":
-    item_reviews = [torch.randint(low=0, high=2, size=(1,80)), torch.randint(low=0, high=2, size=(1,80))]
-    user_reviews = [torch.randint(low=0, high=2, size=(1,80)), torch.randint(low=0, high=2, size=(1,80))]
+    batch_size=5
+    item_reviews = [torch.randint(low=0, high=2, size=(batch_size,80)), torch.randint(low=0, high=2, size=(batch_size,80))]
+    user_reviews = [torch.randint(low=0, high=2, size=(batch_size,80)), torch.randint(low=0, high=2, size=(batch_size,80))]
 
-    review_user_ids = [torch.randint(low=0, high=1, size=(1,1)), torch.randint(low=0, high=1, size=(1,1))]
-    review_item_ids = [torch.randint(low=0, high=2, size=(1, 1)), torch.randint(low=0, high=2, size=(1, 1))]
+    review_user_ids = [torch.randint(low=0, high=1, size=(batch_size,1)), torch.randint(low=0, high=1, size=(batch_size,1))]
+    review_item_ids = [torch.randint(low=0, high=2, size=(batch_size, 1)), torch.randint(low=0, high=2, size=(batch_size, 1))]
 
-    target_user_id = torch.randint(low=0, high = 1, size=(1,1))
-    target_item_id = torch.randint(low=0, high=2, size=(1,1))
+    target_user_id = torch.randint(low=0, high = 1, size=(batch_size,1))
+    target_item_id = torch.randint(low=0, high=2, size=(batch_size,1))
 
 
-    model = KGRAMS(embedding_size = 100, vocab_size = 3 , out_channels = 100, filter_size = 3, review_length=80, user_id_size = 2, item_id_size = 3, embedding_id_size = 100, hidden_size = 50)
+    model = KGRAMS(embedding_size = 100, vocab_size = 3 , out_channels = 100, filter_size = 3, review_length=80, user_id_size = 2, item_id_size = 3, embedding_id_size = 100, hidden_size = 50, latent_size=70)
 
     model(target_user_id = target_user_id,
           user_revs = user_reviews,
