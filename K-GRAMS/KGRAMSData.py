ad0d6c8e3f1595335db0c134bf3478e1721b12bf
Manasa JB <43213734+jbmanasa@users.noreply.github.com>
2019-10-11 17:17:01 +0200

    DataLoader Part

--- K-GRAMS/KGRAMSData.py
+++ K-GRAMS/KGRAMSData.py
@@ -2,6 +2,9 @@ import pickle
 from collections import defaultdict
 import torch
 import copy
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from BaseData import BaseData
 
 PAD_INDEX = 0
 PAD_WORD = '<PAD>'
@@ -15,13 +18,22 @@ END_WORD = '<END>'
 UNK_INDEX = 3
 UNK_WORD = '<UNK>'
 
-class DataLoader():
-    def __init__(self, data_dict, seq_length, word2idx, idx2word):
-        self.unprocessed_data_dict = data_dict
-        self.word2idx = word2idx
-        self.idx2word = idx2word
+class KGRAMSData(Dataset, BaseData):
+    def __init__(self, data_dir, seq_length, mode = "train"):
+        super(KGRAMSData, self).__init__(data_dir)
         #Truncate/Pad review to seq_length
-        self.data_dict, self.vocab_size, self.user_count, self.item_count = self.preprocess_data(data_dict, seq_length)
+        self.data_set = self.get_mode_data(mode)
+        self.data_dict, self.vocab_size, self.user_count, self.item_count = self.preprocess_data(self.data_set, seq_length)
+        self.target_user_ids, self.target_item_ids, self.target_ratings, self.target_reviews, self.user_reviews, self.item_reviews, self.review_user_ids, self.review_item_ids = self.load_data()
+
+    def get_mode_data(self, mode):
+        if mode is "train":
+            data_set = self.train_data
+        elif mode is "validate":
+            data_set = self.val_data
+        else:
+            data_set = self.test_data
+        return data_set
 
     def preprocess_data(self, data_dict_t, seq_length):
         users = []
@@ -46,7 +58,7 @@ class DataLoader():
         vocab_size = len(self.word2idx)
         return data_dict, vocab_size, len(set(users)), len(set(items))
 
-    def shuffle_data(self, data_dict):
+    def get_data_dictionaries(self, data_dict):
         user_review_ids = defaultdict(list)
         item_review_ids = defaultdict(list)
         user_item_data = []
@@ -79,46 +91,54 @@ class DataLoader():
             user_ids_of_item = []
             for review_id in user_review_ids[user_id]:
                 if data_dict[review_id]["itemID"] is not item_id:
-                    one_user_reviews.append(torch.tensor(data_dict[review_id]["review"]))
-                    item_ids_of_user.append(torch.tensor(data_dict[review_id]["itemID"]))
+                    one_user_reviews.append(data_dict[review_id]["review"])
+                    item_ids_of_user.append(data_dict[review_id]["itemID"])
             for review_id in item_review_ids[item_id]:
                 if data_dict[review_id]["userID"] is not user_id:
-                    one_item_reviews.append(torch.tensor(data_dict[review_id]["review"]))
-                    user_ids_of_item.append(torch.tensor(data_dict[review_id]["userID"]))
+                    one_item_reviews.append(data_dict[review_id]["review"])
+                    user_ids_of_item.append(data_dict[review_id]["userID"])
             if len(one_user_reviews) is not 0 and len(one_item_reviews)is not 0:
-                target_user_ids.append(torch.tensor(user_id))
-                target_item_ids.append(torch.tensor(item_id))
-                target_ratings.append(torch.tensor(rating))
-                target_reviews.append(torch.tensor(review))
-                user_reviews.append(torch.stack(one_user_reviews, dim=0).squeeze().view(len(one_user_reviews), -1))
-                item_reviews.append(torch.stack(one_item_reviews, dim=0).squeeze().view(len(one_item_reviews), -1))
-                review_user_ids.append(torch.stack(user_ids_of_item, dim=0))
-                review_item_ids.append(torch.stack(item_ids_of_user, dim=0))
+                target_user_ids.append(user_id)
+                target_item_ids.append(item_id)
+                target_ratings.append(rating)
+                target_reviews.append(review)
+                user_reviews.append(one_user_reviews)
+                item_reviews.append(one_item_reviews)
+                review_user_ids.append(user_ids_of_item)
+                review_item_ids.append(item_ids_of_user)
+
+        self.data_length = len(target_ratings)
 
         return target_user_ids, target_item_ids, target_ratings, target_reviews, user_reviews, \
                item_reviews, review_user_ids, review_item_ids
 
     def load_data(self):
-        user_review_ids, item_review_ids, user_item_data = self.shuffle_data(self.data_dict)
+        user_review_ids, item_review_ids, user_item_data = self.get_data_dictionaries(self.data_dict)
         return self.process_data(self.data_dict, user_review_ids, item_review_ids, user_item_data)
 
+    def __getitem__(self, idx):
+        return self.target_user_ids[idx], self.target_item_ids[idx], self.target_ratings[idx], self.target_reviews[idx], self.user_reviews[idx], self.item_reviews[idx], self.review_user_ids[idx], self.review_item_ids[idx]
+
+    def __len__(self):
+        return self.data_length
+
 if __name__ == "__main__":
-    train_data_path = "./data/Digital_Music_5_train.pkl"
-    val_data_path = "./data/Digital_Music_5_val.pkl"
-    test_data_path = "./data/Digital_Music_5_test.pkl"
+    root_dir = "../data/"
+    data_set_name = "Digital_Music_5.json"
+    data_path = root_dir + data_set_name
+    print("Processing Data - ", data_path)
 
-    train_data = pickle.load(open(train_data_path, "rb"))
-    val_data = pickle.load(open(val_data_path, "rb"))
-    test_data = pickle.load(open(test_data_path, "rb"))
+    dataset = KGRAMSData(data_path, 80, mode = "train")
+    # target_user_ids, target_item_ids, target_ratings, target_reviews, user_reviews, item_reviews, review_user_ids, \
+    #                                                                     review_item_ids = data_loader.load_data()
 
-    data = {}
-    num_of_points = 0
-    for rev_id in train_data.keys():
-        if num_of_points == 100:
-            break
-        data[rev_id] = train_data[rev_id]
-        num_of_points += 1
-    print(len(data))
-    data_loader = DataLoader(data, 80)
-    target_user_ids, target_item_ids, target_ratings, target_reviews, user_reviews, item_reviews, review_user_ids, \
-                                                                        review_item_ids = data_loader.load_data()
+    data_generator = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=0, drop_last=True, timeout=0)
+    #
+    # for data in data_generator:
+    #     for t in data:
+    #         if type(t) is list:
+    #             print(len(t), t[0].size())
+    #             continue
+    #         print(t.size())
+    #     # print(data[0].size())
+    #     break
