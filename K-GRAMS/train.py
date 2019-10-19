cc3278ab5f2ab660c9cd7ec213ed2975e9c324ea
sohisudhir <44899674+sohisudhir@users.noreply.github.com>
2019-10-20 01:32:27 +0200

    Changed according to the final changes in master

--- K-GRAMS/train.py
+++ K-GRAMS/train.py
@@ -5,11 +5,9 @@ from model import KGRAMS
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import numpy as np
-import sys
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print("Device detected: ", device)
-np.random.seed(2017)
+print(device)
 
 def get_one_review_from_batch(word_idx_batch_list, idx2word):
     for batch in word_idx_batch_list:
@@ -38,7 +36,6 @@ def test(config, model, dataset):
                                         item_reviews=item_reviews,
                                         item_ids_of_reviews=review_item_ids,
                                         target_reviews_x=None,
-                                        dataset_object = dataset,
                                         mode = "test")
 
         # print(word_idx_seq)
@@ -52,7 +49,7 @@ def test(config, model, dataset):
         break
 
 
-def validate(config, model, vocab_size, data_generator, dataset_object):
+def validate(config, model, vocab_size, data_generator):
     mse_loss = nn.MSELoss()
     ce_loss = nn.CrossEntropyLoss()
     rating_batch_loss = []
@@ -75,8 +72,7 @@ def validate(config, model, vocab_size, data_generator, dataset_object):
                                                               item_ids=target_item_ids,
                                                               item_reviews=item_reviews,
                                                               item_ids_of_reviews=review_item_ids,
-                                                              target_reviews_x=target_reviews_x,
-                                                              dataset_object = dataset_object)
+                                                              target_reviews_x=target_reviews_x)
         rating_pred_loss = mse_loss(predicted_rating.detach().squeeze(), target_ratings.float())
         review_probabilities = review_probabilities.detach().view(-1, vocab_size)
         review_gen_loss = ce_loss(review_probabilities.detach(), target_reviews_y.view(-1))
@@ -109,7 +105,7 @@ def train(config):
                         num_of_lstm_layers = 1,
                         num_directions = 1,
                         lstm_hidden_dim = 50)
-    kgrams_model = kgrams_model.to(device) 
+    kgrams_model = kgrams_model.to(device)
 
     data_generator_train = DataLoader(dataset_train, batch_size=config.batch_size, shuffle=True, num_workers=0, drop_last=True, timeout=0)
     data_generator_val = DataLoader(dataset_val, batch_size=config.batch_size, shuffle=True, num_workers=0, drop_last=True, timeout=0)
@@ -142,8 +138,7 @@ def train(config):
                                                               item_ids=target_item_ids,
                                                               item_reviews=item_reviews,
                                                               item_ids_of_reviews=review_item_ids,
-                                                              target_reviews_x = target_reviews_x,
-                                                              dataset_object = dataset_train)
+                                                              target_reviews_x = target_reviews_x)
             rating_pred_loss = mse_loss(predicted_rating.squeeze(), target_ratings.float())
             review_probabilities = review_probabilities.view(-1, vocab_size)
             review_gen_loss = crossentr_loss(review_probabilities, target_reviews_y.view(-1))
@@ -153,20 +148,17 @@ def train(config):
             optimizer.step()
             rating_loss.append(rating_pred_loss.item())
             review_loss.append(review_gen_loss.item())
-            # break
         if (epoch % config.eval_freq == 0):
             PATH = "models/model_"+str(epoch)+".pt"
             torch.save(kgrams_model.state_dict(), PATH)
-            avg_rating_loss, avg_rev_loss = validate(config, kgrams_model, dataset_val.vocab_size, data_generator_val, dataset_val)
+            avg_rating_loss, avg_rev_loss = validate(config, kgrams_model, dataset_val.vocab_size, data_generator_val)
             print("-------------EPOCH:",epoch,"----------------------")
             print("TRAIN : Rating Loss - ", np.mean(rating_loss), "LSTM loss - ", np.mean(review_loss))
             print("VALIDATION:   Rating Loss - ", avg_rating_loss, "LSTM loss - ", avg_rev_loss)
             # print("--------------Generating review--------------------")
             # test(config, kgrams_model, dataset_test)
-            # break
-            # sys.exit(1)
             # print("---------------------------------------------------")
-        # break
+
     print("Training Completed!")
     return kgrams_model
 
@@ -175,13 +167,13 @@ def train(config):
 if __name__ == "__main__":
     config = argparse.ArgumentParser()
     config.add_argument('-root', '--root_dir', required=False, type=str, default="../data/", help='Data root direcroty')
-    config.add_argument('-dataset', '--data_set_name', required=False, type=str, default="Musical_Instruments_5.json", help='Dataset')
+    config.add_argument('-dataset', '--data_set_name', required=False, type=str, default="Digital_Music_5.json", help='Dataset')
     config.add_argument('-length', '--review_length', required=False, type=int, default=80,help='Review Length')
     config.add_argument('-batch_size', '--batch_size', required=False, type=int, default=5, help='Batch size')
     config.add_argument('-nlr', '--narre_learning_rate', required=False, type=float, default=0.01, help='NARRE learning rate')
     config.add_argument('-mlr', '--mrg_learning_rate', required=False, type=float, default=0.0003, help='MRG learning rate')
     config.add_argument('-epochs', '--epochs', required=False, type=int, default=2000, help='epochs')
-    config.add_argument('-wembed', '--word_embedding_size', required=False, type=int, default= 1024, help='Word embedding size')
+    config.add_argument('-wembed', '--word_embedding_size', required=False, type=int, default=100, help='Word embedding size')
     config.add_argument('-iembed', '--id_embedding_size', required=False, type=int, default=100, help='Item/User embedding size')
     config.add_argument('-efreq', '--eval_freq', required=False, type=int, default=100,help='Evaluation Frequency')
     config = config.parse_args()
